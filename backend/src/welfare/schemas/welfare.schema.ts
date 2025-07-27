347c080c24ee0c160da275f3759a0e44e0b8116c
noveeda <cheongst5022@gmail.com>
2025-07-28 08:03:43 +0900

    refactore(welfare): welfare 스키마 지급주기 속성 추가

--- backend/src/welfare/schemas/welfare.schema.ts
+++ backend/src/welfare/schemas/welfare.schema.ts
@@ -4,11 +4,11 @@ import { HydratedDocument } from 'mongoose';
 /**
  * 복지 정보 스키마 정의
  *
- * @property {string} sourceType          - 데이터 출처   (예: 중앙부처, 지방자치단체, 민간기관 등)
+ * @property {string} sourceType          - 데이터 출처   (예: 중앙부처, 지방자치단체, 민간단체 등)
  * @property {string} serviceName         - 복지 이름     (예 : 긴급돌봄 지원사업)
  * @property {string} serviceId           - 복지 서비스 ID(예: WLF00005442)
  * @property {string} description         - 서비스 설명   (예 : 질병, 부상, 주 돌봄자의 갑작스러운 부재(사망, 입원 등) 등에 대응해 돌봄 공백을 신속히 보완해 국민의 돌봄불안을 해소합니다.)
- * @property {string[]} provider          - 복지 제공자   (예 : ["보건복지부 사회서비스사업과"])
+ * @property {string} provider          - 복지 제공자   (예 : ["보건복지부 사회서비스사업과"])
  * @property {string[]} targetAudience    - 지원대상      (예 : ["다자녀", "저소득층", "장애인", "여성", "아동", "청소년", "청년", "중장년", "다문화가족"] or "undefined")
  * @property {string[]} lifeCycle         - 신청대상 생애주기 (예: ["영유아", "아동", "청소년", "청년", "중장년", "노년", "임신·출산"])
  * @property {string[]} serviceCategory   - 서비스 분류   (예 : ["일자리", "주거", "복지", "문화", "교육", "건강", "안전"])
@@ -18,20 +18,20 @@ import { HydratedDocument } from 'mongoose';
  */
 @Schema({ timestamps: true, collection: 'welfare' })
 export class Welfare {
-  @Prop({ required: true })
-  sourceType: string; // 데이터 출처 (예: 중앙부처, 지방자치단체, 민간기관 등)
+  @Prop({ required: true, enum: ['중앙부처', '지자체', '민간단체'] })
+  sourceType: string; // 데이터 출처 (예: 중앙부처, 지방자치단체, 민간단체 등)
 
   @Prop({ required: true })
   serviceName: string; // 복지 이름
 
-  @Prop({ required: true, unique: true })
+  @Prop({ required: true, unique: true, default: null })
   serviceId: string; // 복지 서비스 ID (고유 식별자)
 
-  @Prop({ required: true })
+  @Prop({ default: null })
   description: string; // 서비스 설명
 
-  @Prop({ default: [] })
-  provider: string[]; // 복지 제공자 (복수)
+  @Prop({ default: null })
+  provider: string; // 복지 제공자 (복수)
 
   @Prop({ default: [] })
   targetAudience: string[]; // 지원대상 (복수) (예 : ["다자녀", "저소득층", "장애인", "여성", "아동", "청소년", "청년", "중장년", "다문화가족"] or "undefined")
@@ -39,8 +39,11 @@ export class Welfare {
   @Prop({ default: [] })
   lifeCycle: string[]; // 신청대상 생애주기 (복수) (예: ["영유아", "아동", "청소년", "청년", "중장년", "노년"])
 
-  @Prop({ default: [null] })
+  @Prop({ default: null })
   serviceCategory: string[]; // 서비스 분류 (예: ["일자리", "주거", "복지", "문화", "교육", "건강", "안전"])
+
+  @Prop({ default: null })
+  paymentCycle: string; // 지급주기 (예: 월, 연)
 
   @Prop({ default: null })
   contact: string; // 연락처
